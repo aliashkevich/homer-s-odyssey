06c52e01769db8e9d6d31e42cb9bae5dc16816d4
Anastasiya Aliashkevich <a.v.aliashkevich@gmail.com>
2019-06-08 13:42:53 +0200

    added connection config, post method implemented

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const connection = require('../../helpers/db.js');
 
 module.exports = router.post('/signup', function(req, res, next) {
-  res.send('I am in POST signup');
+  const args = {
+    email: req.body.email,
+    password: req.body.password,
+    name: req.body.name,
+    lastname: req.body.lastname,
+  };
+  connection.query('INSERT INTO users SET ?', args, function(
+    error,
+    results,
+    fields,
+  ) {
+    if (error) {
+      res
+        .status(500)
+        .send(error)
+        .end();
+    } else {
+      res.sendStatus(200);
+    }
+  });
 });
