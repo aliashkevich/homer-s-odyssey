98449abf6b71082f35ce2f03fdeea84bde30a00e
Anastasiya Aliashkevich <a.v.aliashkevich@gmail.com>
2019-06-13 14:01:43 +0200

    Removed comments and unused imports

--- back/app.js
+++ back/app.js
@@ -1,34 +1,24 @@
-// I declare all the necessary libraries
-const http = require('http');
-const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
 const app = express();
 const authRouter = require('./routes/auth/auth');
-const connection = require('./helpers/db.js');
 
-// I set up the application
-app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
-// I implement the API part
 app.get('/', (req, res) => {
   res.send('youhou');
 });
 
 app.use('/auth', authRouter);
 
-/// in case of a not found path, I return the 'Not Found' 404 code
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-//I launch the node server
 let server = app.listen(process.env.PORT || 5000, function() {
   console.log('Listening on port ' + server.address().port);
 });
